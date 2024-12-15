c2ce399dc185d25a78a2dad09a0601646a7175e3
Akhdymda <akhd.ymda0428@gmail.com>
2024-12-15 23:10:35 +0900

    月次レポート作成機能の修正 page_selectの参照ファイル名の修正 sanetest_gptの削除

--- page_select.py
+++ page_select.py
@@ -3,7 +3,7 @@ import streamlit as st
 pages = {
     'アプリ': [
         st.Page('medi_app.py', title='ホカンサポ／訪問記録作成用'),
-        st.Page('sanetest_gpt.py', title='ホカンサポ／月次報告書生成用')
+        st.Page('monthly_report.py', title='ホカンサポ／月次報告書生成用')
     ]}
 
 pg = st.navigation(pages)
